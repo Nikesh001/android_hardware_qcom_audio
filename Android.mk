omnirepo/hardware_qcom_audio-caf-msm8937

remove project path map

Android.mk
@@ -1,6 +1,6 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-msm8996)
 
-ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msm8998 sdm660 sdm845 apq8098_latv,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msmcobalt msmfalcon,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 

post_proc/Android.mk
@@ -104,12 +104,12 @@ LOCAL_MODULE:= libvolumelistener
 LOCAL_VENDOR_MODULE := true
 
 LOCAL_C_INCLUDES := \
-        $(call project-path-for,qcom-audio)/hal \
+        hardware/qcom/audio-caf-msm8996/hal \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
         external/tinyalsa/include \
         $(call include-path-for, audio-effects) \
         $(call include-path-for, audio-route) \
-        $(call project-path-for,qcom-audio)/hal/audio_extn \
+        hardware/qcom/audio-caf-msm8996/hal/audio_extn \
         external/tinycompress/include
 
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr

Nikesh001/android_hardware_qcom_audio

Android.mk
@@ -1,6 +1,6 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf-msm8937)
 
-ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msm8998 sdm660 sdm845 apq8098_latv,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter mpq8092 msm8960 msm8226 msm8x26 msm8610 msm8974 msm8x74 apq8084 msm8916 msm8994 msm8992 msm8909 msm8996 msm8952 msm8937 thorium msm8953 msmgold msmcobalt msmfalcon,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 

post_proc/Android.mk
@@ -108,12 +108,12 @@ LOCAL_MODULE:= libvolumelistener
 LOCAL_VENDOR_MODULE := true
 
 LOCAL_C_INCLUDES := \
-        $(call project-path-for,qcom-audio)/hal \
+        hardware/qcom/audio-caf-msm8937/hal \
         $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
         external/tinyalsa/include \
         $(call include-path-for, audio-effects) \
         $(call include-path-for, audio-route) \
-        $(call project-path-for,qcom-audio)/hal/audio_extn \
+        hardware/qcom/audio-caf-msm8937/hal/audio_extn \
         external/tinycompress/include
 
 LOCAL_C_INCLUDES	+= $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include